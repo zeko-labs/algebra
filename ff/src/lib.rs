cccf0e47c71bbcca12dbfee7e9b143d8809148bd
yout <yout@msn.com>
2026-04-22 21:16:37 +0200

    export dump field

--- ff/src/lib.rs
+++ ff/src/lib.rs
@@ -18,6 +18,8 @@ extern crate educe;
 
 #[cfg(feature = "debug-log")]
 extern crate std;
+#[cfg(feature = "debug-log")]
+pub use crate::fields::models::fp::dump_field_counters;
 
 #[macro_use]
 pub mod biginteger;
