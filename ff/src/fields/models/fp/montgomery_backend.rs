693bd5cd39adb2c30e1ca56541da42ed104fe741
eddy <eddyboughioul@hotmail.fr>
2026-04-28 17:32:31 +0200

    comment mul assig

--- ff/src/fields/models/fp/montgomery_backend.rs
+++ ff/src/fields/models/fp/montgomery_backend.rs
@@ -673,7 +673,7 @@ impl<T: MontConfig<N>, const N: usize> FpConfig<N> for MontBackend<T, N> {
     fn mul_assign(a: &mut Fp<Self, N>, b: &Fp<Self, N>) {
         #[cfg(feature = "debug-log")]
         {
-            std::println!("mul assign montgomery");
+            // std::println!("mul assign montgomery");
         }
         #[cfg(target_os = "zkvm")]
         if N == 4 {
